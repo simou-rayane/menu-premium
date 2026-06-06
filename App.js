f5580792cd75f11f401e7c6daeeac080fa796dfe
simou-rayane <magicsimou@gmail.com>
2026-06-06 21:17:48 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,202 @@
+let data;
+let panier={};
 
+fetch("menu.json")
+.then(r=>r.json())
+.then(json=>{
+
+data=json;
+
+document.getElementById(
+"restaurant-name"
+).innerText=
+data.restaurant.nom;
+
+afficherMenu();
+
+});
+
+function afficherMenu(){
+
+let html="";
+
+data.categories.forEach(cat=>{
+
+html+=`
+<h2 style="padding:15px">
+${cat.emoji} ${cat.nom}
+</h2>
+`;
+
+cat.produits.forEach(p=>{
+
+html+=`
+
+<div class="produit">
+
+<img src="${p.image}">
+
+<div class="info">
+
+<h3>${p.nom}</h3>
+
+<p>${p.description}</p>
+
+<div class="prix">
+${p.prix} DH
+</div>
+
+<div class="actions">
+
+<button
+class="plus"
+onclick="ajouter(
+'${p.nom}',
+${p.prix}
+)">
++
+</button>
+
+<button
+class="moins"
+onclick="retirer(
+'${p.nom}'
+)">
+-
+</button>
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+});
+
+document
+.getElementById("menu")
+.innerHTML=html;
+
+}
+
+function ajouter(nom,prix){
+
+if(!panier[nom]){
+
+panier[nom]={
+qte:0,
+prix:prix
+};
+
+}
+
+panier[nom].qte++;
+
+refresh();
+
+}
+
+function retirer(nom){
+
+if(!panier[nom]) return;
+
+panier[nom].qte--;
+
+if(
+panier[nom].qte<=0
+){
+delete panier[nom];
+}
+
+refresh();
+
+}
+
+function refresh(){
+
+let html="";
+let total=0;
+
+for(let nom in panier){
+
+let q=
+panier[nom].qte;
+
+let p=
+panier[nom].prix;
+
+total+=q*p;
+
+html+=`
+<div>
+${nom}
+x${q}
+=
+${q*p} DH
+</div>
+`;
+
+}
+
+if(html===""){
+html="Panier vide";
+}
+
+document
+.getElementById(
+"panier-contenu"
+)
+.innerHTML=html;
+
+document
+.getElementById(
+"total"
+)
+.innerText=total;
+
+}
+
+function commander(){
+
+let total=0;
+
+let msg=
+"Bonjour,%0A%0ACommande :%0A";
+
+for(let nom in panier){
+
+let q=
+panier[nom].qte;
+
+let p=
+panier[nom].prix;
+
+total+=q*p;
+
+msg+=
+"- "+nom+
+" x"+q+
+"%0A";
+
+}
+
+msg+=
+"%0ATotal : "+
+total+
+" DH";
+
+window.open(
+"https://wa.me/"
++
+data.restaurant.telephone
++
+"?text="
++
+msg
+);
+
+}
